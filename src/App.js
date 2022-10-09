fc0c1f718b1153f493190e2edb0cc886002e25e9
frf-12 <fauzirahmanf@gmail.com>
2022-10-09 17:23:23 +0700

    tes pertama

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import './App.css';
+import React, { useState } from "react";
+import { CssBaseline, Grid, Paper } from "@mui/material";
+import "./App.css";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
+import ContactForm from "./components/ContactForm";
 
 // Uncomment untuk memuat daftar kontak
-// import contactsJSON from './data/contacts.json';
+import contactsJSON from "./data/contacts.json";
 const App = () => {
   // Masukkan Header dan lakukan map untuk Contact ke dalam div App
   // untuk membuat daftar kontak bisa menggunakan MUI list
@@ -10,8 +15,25 @@ const App = () => {
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
 
+  const [contact, setContact] = useState(contactsJSON);
+
+  const addContact = (newContact) => {
+    setContact([...contact, newContact]);
+  };
+
   return (
     <div className="App">
+      <Header />
+      <br />
+      <Grid container sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid item xs={12} sm={6} component={Paper} elevation={3}>
+          <ContactForm add={addContact} />
+        </Grid>
+        <Grid item xs={12} sm={6} component={Paper} elevation={3}>
+          <Contact data={contact} />
+        </Grid>
+      </Grid>
     </div>
   );
 };
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,14 +2,119 @@
 // https://mui.com/material-ui/react-text-field/#basic-textfield
 // dan Card
 // https://mui.com/material-ui/react-card/#basic-card
+import * as React from "react";
+import {
+  Grid,
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  TextField,
+} from "@mui/material";
 
-const ContactForm = () => {
-    // Form berisi name, phone, email, dan photo url
-    // Buatlah state newContact berupa objek sesuai dengan data yang ada
+const ContactForm = (props) => {
+  // Form berisi name, phone, email, dan photo url
+  // Buatlah state newContact berupa objek sesuai dengan data yang ada
 
-    return (
-        <> </>
-    );
-}
+  const [newContact, setNewContact] = React.useState({
+    name: "",
+    phone: "",
+    email: "",
+    photo: "",
+  });
+
+  const onInputChange = (event) => {
+    setNewContact((kontak) => ({
+      ...kontak,
+      [event.target.id]: event.target.value,
+    }));
+  };
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    props.add(newContact);
+    setNewContact({
+      name: "",
+      phone: "",
+      email: "",
+      photo: "",
+    });
+  };
+
+  return (
+    <>
+      <Box component="form" noValidate onSubmit={submitForm} sx={{ mt: 3 }}>
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Grid container>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="name"
+                  type="text"
+                  label="Name"
+                  id="name"
+                  size="small"
+                  variant="filled"
+                  value={newContact.name}
+                  onChange={onInputChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="phone"
+                  type="text"
+                  label="Phone"
+                  id="phone"
+                  size="small"
+                  onChange={onInputChange}
+                  value={newContact.phone}
+                  variant="filled"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="email"
+                  type="text"
+                  label="Email"
+                  id="email"
+                  size="small"
+                  onChange={onInputChange}
+                  value={newContact.email}
+                  variant="filled"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="photo"
+                  type="text"
+                  label="Photo Url"
+                  id="photo"
+                  size="small"
+                  onChange={onInputChange}
+                  value={newContact.photo}
+                  variant="filled"
+                />
+              </Grid>
+            </Grid>
+          </CardContent>
+          <CardActions>
+            <Button fullWidth size="small" type="submit" variant="contained">
+              Submit
+            </Button>
+          </CardActions>
+        </Card>
+      </Box>
+    </>
+  );
+};
 
 export default ContactForm;
